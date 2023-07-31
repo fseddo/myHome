4282f0086b51e579076a8a76bbd5fc91e4eca5aa
Francesco <francesco.seddo@gmail.com>
2023-07-31 19:39:46 -0400

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import Paper from "@mui/material/Paper";
+import { ViewState } from "@devexpress/dx-react-scheduler";
+import {
+  Scheduler,
+  MonthView,
+  Appointments,
+} from "@devexpress/dx-react-scheduler-material-ui";
 
 function App() {
+  const currentDate = new Date();
+  const schedulerData = [
+    {
+      startDate: "2023-08-01T09:45",
+      endDate: "2023-08-02T11:00",
+      title: "Andrew and Francesco at Beyonce",
+    },
+    {
+      startDate: "2018-11-01T12:00",
+      endDate: "2018-11-01T13:30",
+      title: "Go to a gym",
+    },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Paper>
+        <Scheduler data={schedulerData}>
+          <ViewState currentDate={currentDate} />
+          <MonthView />
+          <Appointments />
+        </Scheduler>
+      </Paper>
     </div>
   );
 }
